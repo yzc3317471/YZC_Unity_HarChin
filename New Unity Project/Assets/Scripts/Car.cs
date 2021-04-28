2f3630ccf924bc3bb72dffb7bcfcd53298cf8255
yzc3317471 <82381539+yzc3317471@users.noreply.github.com>
2021-04-28 21:31:28 +0800

    C#語言講解

---
+++ New Unity Project/Assets/Scripts/Car.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//單行註解 SOSO 2021.04.28
+
+/*
+ * 多行註解
+ * 多行註解
+ * /
+
+ /*欄位
+  * 整數:int
+  * 浮點數:float
+  * 字串:string
+  * 布林值:bool
+  * ex: int test；(一個整數叫test)
+  */
+public int steing = 10;
+public int cc = 2000;
+public float weight = 3.5f;
+public string brand = "賓士";
+public bool openCold = false;
+
