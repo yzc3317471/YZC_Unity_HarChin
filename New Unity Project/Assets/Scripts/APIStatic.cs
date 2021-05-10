b8c4dcb134f4e44c6128e1360e3a7dc0451fc0fd
yzc3317471 <82381539+yzc3317471@users.noreply.github.com>
2021-05-10 20:12:22 +0800

    靜態方法

---
+++ New Unity Project/Assets/Scripts/APIStatic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+partial class APIStatic:MonoBehaviour
+    {
+        private void Start()
+        {
+            //使用靜態API
+            //屬性
+            //可以取得Get、可以設定Set
+            //如果看到Read Only 只能取得
+
+            //1.取得靜態的屬性
+            //語法:類別,靜態屬性
+            print("隨機" + Random.value);
+
+            //2.設定靜態的屬性
+            //語法:類別.靜態屬性 指定 值;
+            Cursor.visible = false;
+
+            int r = Random.Range(50, 150);
+            print ("隨機攻擊力:"+r)
+        }
+
+
+    }
+ 
+
+
+
+
+
+}
--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -4,11 +4,63 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region 欄位
+    [Header("跳躍高度"), Range(0, 3000)]
+    public int jump = 100;
+    [Header("血量"), Range(0, 2000)]
+    public float hp = 500;
+    [Header("是否地板上")]
+    public bool isGround = false;
+
+    private int score;
+    private AudioClip sound1;
+    private AudioClip sound2;
+    private AudioSource aud;
+    private Rigidbody2D rig;
+    private Animator ani;
+    #endregion
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    #region 方法
+    private void Jump()
+    {
+
+    }
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Atteck()
+    {
+
+    }
+
+    /// <summary>
+    ///受傷 
+    /// </summary>
+    /// <param name="damage">收到的傷害</param>
+    private void Hit(float damage)
+    {
+
+    }
     
+    
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private bool Dead()
+    {
+        return false;
+    }
 
+    /// <summary>
+    /// 加分
+    /// </summary>
+    /// <param name="add"></param>
+    private void AddScore(int add)
+    {
 
-
-
+    }
 
 
 }
